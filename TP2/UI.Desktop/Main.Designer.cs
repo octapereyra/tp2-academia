4dc25fa8c73e8ecca43b3c24e0213d05e5c1f6d8
euge0133 <101753721+euge0133@users.noreply.github.com>
2022-06-23 10:59:37 -0300

    menu principal temporal

---
+++ TP2/UI.Desktop/Main.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace UI.Desktop
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.mnuArchivo = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAbrir = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPersonas = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuMaterias = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuUsuarios = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuPrincipal
+            // 
+            this.mnuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuArchivo});
+            this.mnuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.mnuPrincipal.Name = "mnuPrincipal";
+            this.mnuPrincipal.Size = new System.Drawing.Size(800, 24);
+            this.mnuPrincipal.TabIndex = 0;
+            this.mnuPrincipal.Text = "menuStrip1";
+            // 
+            // mnuArchivo
+            // 
+            this.mnuArchivo.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuAbrir});
+            this.mnuArchivo.Name = "mnuArchivo";
+            this.mnuArchivo.Size = new System.Drawing.Size(60, 20);
+            this.mnuArchivo.Text = "Archivo";
+            // 
+            // mnuAbrir
+            // 
+            this.mnuAbrir.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuPersonas,
+            this.mnuMaterias,
+            this.mnuUsuarios});
+            this.mnuAbrir.Name = "mnuAbrir";
+            this.mnuAbrir.Size = new System.Drawing.Size(100, 22);
+            this.mnuAbrir.Text = "Abrir";
+            // 
+            // mnuPersonas
+            // 
+            this.mnuPersonas.Name = "mnuPersonas";
+            this.mnuPersonas.Size = new System.Drawing.Size(121, 22);
+            this.mnuPersonas.Text = "Personas";
+            this.mnuPersonas.Click += new System.EventHandler(this.mnuPersonas_Click);
+            // 
+            // mnuMaterias
+            // 
+            this.mnuMaterias.Name = "mnuMaterias";
+            this.mnuMaterias.Size = new System.Drawing.Size(121, 22);
+            this.mnuMaterias.Text = "Materias";
+            this.mnuMaterias.Click += new System.EventHandler(this.mnuMaterias_Click);
+            // 
+            // mnuUsuarios
+            // 
+            this.mnuUsuarios.Name = "mnuUsuarios";
+            this.mnuUsuarios.Size = new System.Drawing.Size(121, 22);
+            this.mnuUsuarios.Text = "Usuarios";
+            this.mnuUsuarios.Click += new System.EventHandler(this.mnuUsuarios_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mnuPrincipal);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.mnuPrincipal;
+            this.Name = "frmMain";
+            this.Text = "Academia";
+            this.mnuPrincipal.ResumeLayout(false);
+            this.mnuPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem mnuArchivo;
+        private System.Windows.Forms.ToolStripMenuItem mnuAbrir;
+        private System.Windows.Forms.ToolStripMenuItem mnuPersonas;
+        private System.Windows.Forms.ToolStripMenuItem mnuMaterias;
+        private System.Windows.Forms.ToolStripMenuItem mnuUsuarios;
+    }
+}
---
+++ TP2/UI.Desktop/Main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI.Desktop
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuPersonas_Click(object sender, EventArgs e)
+        {
+            frmPersonas p = new frmPersonas();
+            p.ShowDialog();
+        }
+
+        private void mnuMaterias_Click(object sender, EventArgs e)
+        {
+            frmMaterias m = new frmMaterias();
+            m.ShowDialog();
+        }
+
+        private void mnuUsuarios_Click(object sender, EventArgs e)
+        {
+            frmUsuarios u = new frmUsuarios();
+            u.ShowDialog();
+        }
+    }
+}
