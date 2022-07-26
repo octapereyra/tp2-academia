ff1339365b5cd3e9eb0cd99c0ec682f68f56ed9e
euge0133 <101753721+euge0133@users.noreply.github.com>
2022-07-26 16:07:49 -0300

    Add files via upload

---
+++ TP2/Data.Database/ComisionAdapter.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Data.Database
+{
+    public class ComisionAdapter : Adapter
+    {
+
+        public List<Comision> GetAll()
+        {
+            List<Comision> comisiones = new();
+            try
+            {
+                this.OpenConnection();
+
+                SqlCommand cmdComisiones = new SqlCommand("select * from comisiones", SqlConn);
+                SqlDataReader drComisiones = cmdComisiones.ExecuteReader();
+
+                while (drComisiones.Read())
+                {
+                    Comision com = new Comision();
+                    com.ID = (int)drComisiones["id_comision"];
+                    com.Descripcion = (string)drComisiones["desc_comision"];
+                    com.AnioEspecialidad = (int)drComisiones["anio_especialidad"];
+                    com.IDPlan = (int)drComisiones["id_plan"];
+
+                    comisiones.Add(com);
+                }
+
+                drComisiones.Close();
+
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada =
+                new Exception("Error al recuperar lista de comisiones", Ex);
+                throw ExcepcionManejada;
+            }
+
+            finally
+            {
+                this.CloseConnection();
+            }
+
+
+            return comisiones;
+        }
+
+        public Comision GetOne(int ID)
+        {
+            Comision com = new Comision();
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdComisiones = new SqlCommand("select * from comisiones where id_comision=@id", SqlConn);
+                cmdComisiones.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+                SqlDataReader drComisiones = cmdComisiones.ExecuteReader();
+                if (drComisiones.Read())
+                {
+                    com.ID = (int)drComisiones["id_comision"];
+                    com.Descripcion = (string)drComisiones["desc_comision"];
+                    com.AnioEspecialidad = (int)drComisiones["anio_especialidad"];
+                    com.IDPlan = (int)drComisiones["id_plan"];
+                }
+
+                drComisiones.Close();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al recuperar datos de la comision", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+            return com;
+        }
+
+        public void Delete(int ID)
+        {
+
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdDelete = new SqlCommand("delete comisiones where id_comision=@id", SqlConn);
+                cmdDelete.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+                cmdDelete.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al eliminar una comision", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        protected void Insert(Comision com)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdSave = new SqlCommand("INSERT INTO comisiones(desc_especialidad,anio_especialidad,id_plan)" +
+                    " VALUES (@desc_comision,@anio_especialidad,@id_plan)" +
+                    " SELECT @@identity", SqlConn);
+
+                cmdSave.Parameters.Add("@desc_comision", SqlDbType.VarChar, 50).Value = com.Descripcion;
+                com.ID = Decimal.ToInt32((decimal)cmdSave.ExecuteScalar());
+                cmdSave.Parameters.Add("@anio_especialidad", SqlDbType.Int).Value = com.AnioEspecialidad;
+                cmdSave.Parameters.Add("@id_plan", SqlDbType.Int).Value = com.IDPlan;
+            }
+            catch (Exception Ex)
+            {
+                Exception ExceptionManejada = new Exception("Error al insertar la comision", Ex);
+                throw ExceptionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        protected void Update(Comision com)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdSave = new SqlCommand("UPDATE comisiones SET " +
+                    "desc_comision=@desc_comision, anio_especialidad=@anio_especialidad, id_plan=@id_plan WHERE id_comision=@id", SqlConn);
+                cmdSave.Parameters.Add("@id", SqlDbType.Int).Value = com.ID;
+                cmdSave.Parameters.Add("@desc_comision", SqlDbType.VarChar, 50).Value = com.Descripcion;
+                cmdSave.Parameters.Add("@anio_especialidad", SqlDbType.Int).Value = com.AnioEspecialidad;
+                cmdSave.Parameters.Add("@id_plan", SqlDbType.Int).Value = com.IDPlan;
+
+                cmdSave.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExceptionManejada = new Exception("Error al modificar datos de la comision", Ex);
+                throw ExceptionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        public void Save(Comision com)
+        {
+
+            if (com.State == BusinessEntity.States.Deleted)
+            {
+                Delete(com.ID);
+            }
+            else if (com.State == BusinessEntity.States.New)
+            {
+                Insert(com);
+            }
+            else if (com.State == BusinessEntity.States.Modified)
+            {
+                Update(com);
+            }
+            com.State = BusinessEntity.States.Unmodified;
+        }
+    }
+}
