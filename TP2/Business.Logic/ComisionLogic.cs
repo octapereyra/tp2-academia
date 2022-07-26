04254dbb8dea3d431621329f787bae4c2f2a26d7
euge0133 <101753721+euge0133@users.noreply.github.com>
2022-07-26 16:07:11 -0300

    Add files via upload

---
+++ TP2/Business.Logic/ComisionLogic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using Data.Database;
+
+namespace Business.Logic
+{
+    public class ComisionLogic : BusinessLogic
+    {
+        public ComisionLogic()
+        {
+            ComisionData = new ComisionAdapter();
+        }
+
+        ComisionAdapter _ComisionData;
+
+        public ComisionAdapter ComisionData { get => _ComisionData; set => _ComisionData = value; }
+
+        public Comision GetOne(int ID)
+        {
+            return ComisionData.GetOne(ID);
+        }
+
+        public List<Comision> GetAll()
+        {
+            return ComisionData.GetAll();
+        }
+
+        public void Save(Comision com)
+        {
+            ComisionData.Save(com);
+        }
+
+        public void Delete(int ID)
+        {
+            ComisionData.Delete(ID);
+        }
+    }
+}
