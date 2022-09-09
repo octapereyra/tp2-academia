8de62e8b4645e097822bcbd6e073e5e36f02f324
octapereyra <octaviopereyra19@gmail.com>
2022-09-09 10:30:20 -0300

    creacion master page

--- TP2/UI.Web/Controllers/HomeController.cs
+++ TP2/UI.Web/Controllers/HomeController.cs
@@ -33,5 +33,11 @@ namespace UI.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public ActionResult PaginaTest()
+        {
+
+            return View();
+        }
     }
 }
