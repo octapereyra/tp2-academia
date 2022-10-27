92af88c2df97e0b6e875c9fae435c80452dff95a
octapereyra <octaviopereyra19@gmail.com>
2022-10-27 17:14:58 -0300

    agregada inscripcion a materia

--- TP2/UI.Web/Controllers/InscripcionController.cs
+++ TP2/UI.Web/Controllers/InscripcionController.cs
@@ -15,27 +15,18 @@ namespace UI.Web.Controllers
             return View();
         }
 
-        public JsonResult GetPlanes(int id_especialidad)
-        {
-            IEnumerable<Plan> planes = new PlanLogic().GetAll();
-            planes = planes.Where(p => p.IDEspecialidad == id_especialidad);
-
-            return Json(planes);
-        }
-        public JsonResult GetMaterias(int id_plan)
-        {
-            IEnumerable<Materia> materias = new MateriaLogic().GetAll();
-            materias = materias.Where(m => m.IDPlan == id_plan);
-
-            return Json(materias);
-        }
-
-        public JsonResult GetCursos(int id_materia)
+        public JsonResult GetComisiones(int id_materia)
         {
             IEnumerable<Curso> cursos = new CursoLogic().GetAll();
             cursos = cursos.Where(c => c.IDMateria == id_materia && c.Cupo > 0);
+            List<Comision> comisiones = new(); 
+            foreach (Curso curso in cursos)
+            {
+                comisiones.Add(new ComisionLogic().GetOne(curso.IDComision));
+            }
 
-            return Json(cursos);
+            return Json(comisiones);
         }
+
     }
 }
--- TP2/UI.Web/Controllers/RegistroController.cs
+++ TP2/UI.Web/Controllers/RegistroController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Entities;
+using Business.Logic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +12,35 @@ namespace UI.Web.Controllers
     public class RegistroController : Controller
     {
         [HttpPost]
-        public IActionResult Inscribir()
+        public ActionResult Inscribir(Curso curso)
         {
+            try
+            {
+            Usuario alumno = new UsuarioLogic().GetOne((int)HttpContext.Session.GetInt32("userId"));
+            IEnumerable<Curso> cursos = new CursoLogic().GetAll();
+            cursos = cursos.Where(c => c.IDComision == curso.IDComision && c.IDMateria == curso.IDMateria);
+            curso = cursos.First();
+            AlumnoInscripcion inscripcion = new()
+            {
+                IDCurso = curso.ID,
+                IDAlumno = alumno.IdPersona,
+                Condicion = "Inscripto",
+                Nota = 0,
+                State = BusinessEntity.States.New,
+            };           
+                new InscripcionLogic().Save(inscripcion);
+                curso.Cupo -= 1;
+                curso.State = BusinessEntity.States.Modified;
+                new CursoLogic().Save(curso);
+                Comision com = new ComisionLogic().GetOne(curso.IDComision);
+                Materia mat = new MateriaLogic().GetOne(curso.IDMateria);
+                ViewData["msj"] = "Inscripción realizada con éxito. Te inscribiste en la materia " + mat.Descripcion + " en la comisión " + com.Descripcion;
+            }
+            catch(Exception ex)
+            {
+                ViewData["msj"] = ex.Message;
+            }
+
             return View();
         }
     }
